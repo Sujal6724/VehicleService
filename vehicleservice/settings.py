8b35b9a2129ac7a3a8b98204d479328b3ded524e
NIKHIL GUDURU <gudurunikhil123@gmail.com>
2026-03-02 17:09:27 +0530

    Completed complaints module with validation and testing

--- vehicleservice/settings.py
+++ vehicleservice/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'core',
     'partners',
     'services',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+AUTH_USER_MODEL='accounts.user'
